fb03816e76766c59c4e6105362ebc86001b9364a
Tsigabuwalker <tsigabu.seyoum-ug@aau.edu.et>
2025-02-07 19:54:15 +0300

    adding some python code

--- cs50.py
+++ cs50.py
@@ -4,5 +4,5 @@
 # python allows you to write programs quickly 
 #python is a powerful programming language
 # python is a programming language that is easy to learn
-name = input("What is your name?")
-print("Hello," + name)
+name = input("What is your name? ")
+print(f"Hello, {name}")
