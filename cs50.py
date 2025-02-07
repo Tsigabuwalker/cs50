f9685aab5d7af09d364e236ed22dc2cd3686d4e4
Tsigabuwalker <tsigabu.seyoum-ug@aau.edu.et>
2025-02-07 19:47:54 +0300

    some python code

---
+++ cs50.py
@@ -0,0 +1,8 @@
+# python is an interpreter programming languages
+#python is a high-lever programming language
+#python is a general-purpose programming language
+# python allows you to write programs quickly 
+#python is a powerful programming language
+# python is a programming language that is easy to learn
+name = input("What is your name?")
+print("Hello," + name)
