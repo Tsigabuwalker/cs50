7495b99692f674c1a41a2e01d343bbeb85980341
Tsigabuwalker <tsigabu.seyoum-ug@aau.edu.et>
2025-02-07 20:53:42 +0300

    some python code

---
+++ dect.py
@@ -0,0 +1,2 @@
+houses = {"n1":"tsigbau", "n2":"Abera","n3":"Hagos"}
+print(houses["n1"])
---
+++ loops.py
@@ -0,0 +1,7 @@
+names = ["batasha", "Guya", "nigga", "kingo","aburaya","hagos"]
+for num in names:
+    print(num)
+
+department = "Computer Science"
+for dep in department:
+    print(dep)
